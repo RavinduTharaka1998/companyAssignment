6067bbfbd6f56479866d6411c40c95e3c2a9684c
RavinduTharaka1998 <68723931+RavinduTharaka1998@users.noreply.github.com>
2023-05-16 20:12:32 +0530

    create database part

---
+++ companyAssignment/Migrations/20230516143804_initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace companyAssignment.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    lastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    dob = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    age = table.Column<int>(type: "int", nullable: false),
+                    salary = table.Column<float>(type: "real", nullable: false),
+                    department = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- companyAssignment/Program.cs
+++ companyAssignment/Program.cs
@@ -1,7 +1,16 @@
+using companyAssignment.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<EmployeeContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
+
 builder.Services.AddRazorPages();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -12,6 +21,16 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
