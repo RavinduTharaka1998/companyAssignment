359e54d106e74656b55fb6114656a635bbce6be9
RavinduTharaka1998 <68723931+RavinduTharaka1998@users.noreply.github.com>
2023-05-16 17:27:13 +0530

    add employee model

---
+++ companyAssignment/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace companyAssignment.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public DateTime dob { get; set; }
+        public int age { get; set; }
+        public float salary { get; set; }
+        public string department { get; set; }
+    }
+}
